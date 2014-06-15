08717fc0cf827f08a9751179c41fc8cbaa6fcf1e
robertdirosa <robert@di-rosa.ch>
2014-06-15 19:32:41 +0200

    seed v0.2 squelette
    squelette semble terminé

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -146,10 +146,33 @@ class RessourcesTableSeeder extends Seeder {
     }
 }
 
+
+//printings_types
+class PrintingsTypesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('printings_types')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//functions
+class FunctionsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('functions')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
 /**
 *
 *
-*		seed des tables pouvant vivres seules 
+*		seed des tables pouvant vivre seules 
 *
 *
 **/
@@ -204,6 +227,167 @@ class GroupsTableSeeder extends Seeder {
     public function run()
     {
         DB::table('groups')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//members
+class MembersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('members')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+
+/**
+*
+*
+*		Deuxième périphérie (définition de bob )
+*
+*
+**/
+
+//artists_genres
+class ArtistsGenresTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('artists_genres')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//publications
+class PublicationsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('publications')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//tickets
+class TicketssTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('tickets')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//attributions
+class AttributionsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('attributions')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//offers
+class OffersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('offers')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//printings
+class PrintingsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('printings')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//function_member
+class FunctionMembersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('function_member')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+
+/** 
+*
+*
+* 		Pivot tables 
+*
+*
+**/
+
+//events_needs
+class EventNeedsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('events_needs')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//staffs
+class StaffsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('staffs')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//group_ressources
+class GroupRessourcesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('group_ressources')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//artists_musicains
+class ArtistMusicianTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('artists_musicains')->delete();
+
+        User::create(array('email' => 'foo@bar.com'));
+    }
+}
+
+//performers
+class PerfomrmersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('performers')->delete();
 
         User::create(array('email' => 'foo@bar.com'));
     }
